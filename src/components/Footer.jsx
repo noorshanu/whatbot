57d6de08bee469c56b023b9071a616ad22034d5f
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-01 17:38:32 +0530

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { AiOutlineTwitter } from 'react-icons/ai'
+import { BiLogoTelegram } from "react-icons/bi";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
   <section>
-    <footer className=' text-center'>
+    <footer className=' text-center mt-3 sm:mt-8'>
         <img src="images/logo.png" alt="" className=' mx-auto' />
 
-        <div className='flex'>
-            <a href="/"><AiOutlineTwitter/></a>
-            <a href="/"><AiOutlineTwitter/></a>
-            <a href="/"><AiOutlineTwitter/></a>
+        <div className='flex justify-center items-center gap-6 py-5'>
+            <a href="/" className=' text-3xl text-white hover:text-[#3557d5] '><AiOutlineTwitter/></a>
+            <a href="/" className=' text-3xl text-white hover:text-[#3557d5] '><BiLogoTelegram /></a>
+            <a href="/" className=' text-3xl text-white hover:text-[#3557d5]'><FaInstagram /></a>
         </div>
-        <p>
+        <p className=' text-[#747474] text-sm font-semibold'>
         Copyright WhatBot - All right reserved
         </p>
     </footer>
--- src/components/JoinNow.jsx
+++ src/components/JoinNow.jsx
@@ -4,11 +4,11 @@ function JoinNow() {
   return (
    <section>
     <div className='container-wrapper'>
-        <div className='joinus py-5 text-center'>
-            <h1 >Join The WHATBOT Community</h1>
+        <div className='joinus py-12 text-center my-5'>
+            <h1 className=' text-3xl sm:text-6xl font-bitsu ' >Join The WHATBOT Community</h1>
 
-            <p className=' font-poppins pb-6 pt-4'>Meet the community of air drop farmers</p>
-            <a href="/" className="btn-main px-14 py-2">Join The Community</a>
+            <p className=' font-poppins pt-6 pb-10  text-[#8b8b8b]'>Meet the community of air drop farmers</p>
+            <a href="/" className="btn-main px-16 py-4 mt-6 font-bitsu text-4xl">Join The Community</a>
 
         </div>
 

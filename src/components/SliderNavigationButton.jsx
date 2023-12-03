542645bdf4fa09a252b06988c7f4fdbe0c4e1e95
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-03 23:40:28 +0530

    new

--- src/components/SliderNavigationButton.jsx
+++ src/components/SliderNavigationButton.jsx
@@ -6,7 +6,7 @@ function SliderNavigationButton({ className, ...props }) {
     <button
       {...props}
       className={twMerge(
-        "w-14 h-14 rounded-full bg-black/40 flex items-center justify-center text-2xl disabled:bg-[#485f95]",
+        "w-14 h-14 rounded-full bg-[#485f95]/40 flex items-center justify-center text-2xl disabled:bg-[#485f95]",
         className
       )}
     >

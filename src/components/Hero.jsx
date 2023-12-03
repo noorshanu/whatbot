7492dc1fbec1e9eaa2a2e4b603ee9eb41fdcff37
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-04 00:08:29 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiOutlineTwitter } from "react-icons/ai";
 import { BiLogoTelegram } from "react-icons/bi";
 import { FaInstagram } from "react-icons/fa";
 import { FaMedium } from "react-icons/fa6";
@@ -9,38 +9,73 @@ function Hero() {
       <div className="container-wrapper">
         <div className=" flex justify-start items-center flex-col sm:flex-row ">
           <div className=" max-w-xl mt-[15%] sm:mt-[23%]">
-            <h1 className=" text-white font-bitsu text-3xl sm:text-5xl font-medium pb-2">Unleash Your Crypto Potential With Ease !</h1>
+            <h1 className=" text-white font-bitsu text-3xl sm:text-5xl font-medium pb-2">
+              Unleash Your Crypto Potential With Ease !
+            </h1>
             <p className=" font-poppins text-base sm:text-xl font-medium pb-4">
-            WhatBot is here to empower your crypto
- journey. Whether you're a newbie or a seasoned pro,
- we've got your back. Our user-friendly platform simplifies 
-token creation and management, ensuring
- you have the keys to unlock your crypto 
-potential. Explore the possibilities with
- WhatBot by your side.
+              WhatBot is here to empower your crypto journey. Whether you're a
+              newbie or a seasoned pro, we've got your back. Our user-friendly
+              platform simplifies token creation and management, ensuring you
+              have the keys to unlock your crypto potential. Explore the
+              possibilities with WhatBot by your side.
             </p>
 
             <div className=" py-4 ">
-                <a href="/" className="btn-main px-7 sm:px-14 py-3 mr-4 font-bitsu text-base sm:text-xl">Buy Now</a>
-                <a href="/" className="btn-main px-7 sm:px-14 py-3 font-bitsu text-base sm:text-xl" > Join us</a>
+              <a
+                href="/"
+                className="btn-main px-7 sm:px-14 py-3 mr-4 font-bitsu text-base sm:text-xl"
+              >
+                Buy Now
+              </a>
+              <a
+                href="/"
+                className="btn-main px-7 sm:px-14 py-3 font-bitsu text-base sm:text-xl"
+              >
+                {" "}
+                Join us
+              </a>
             </div>
 
-            <div className='flex justify-start items-center gap-6 py-5'>
-            <a href="/" className=' text-3xl text-white hover:text-[#3557d5] '><AiOutlineTwitter/></a>
-            <a href="/" className=' text-3xl text-white hover:text-[#3557d5] '><BiLogoTelegram /></a>
-            <a href="/" className=' text-3xl text-white hover:text-[#3557d5]'><FaInstagram /></a>
-            <a href="/" className=' text-3xl text-white hover:text-[#3557d5]'><FaMedium /></a>
-        </div>
+            <div className="flex justify-start items-center gap-6 py-5">
+              <a
+                href="/"
+                className=" text-3xl text-white hover:text-[#3557d5] "
+              >
+                <AiOutlineTwitter />
+              </a>
+              <a
+                href="/"
+                className=" text-3xl text-white hover:text-[#3557d5] "
+              >
+                <BiLogoTelegram />
+              </a>
+              <a href="/" className=" text-3xl text-white hover:text-[#3557d5]">
+                <FaInstagram />
+              </a>
+              <a href="/" className=" text-3xl text-white hover:text-[#3557d5]">
+                <FaMedium />
+              </a>
+            </div>
           </div>
           <div className=" relative">
-            <img src="images/coin-1.png" alt="" className=" mt-0 sm:mt-[0rem] floating" />
-            <img src="images/bot.svg" alt="" className="  h-[300px] block sm:hidden floating" />
-
+            <img
+              src="images/coin-1.png"
+              alt=""
+              className=" mt-0 sm:mt-[0rem] floating"
+            />
+            <img
+              src="images/bot.svg"
+              alt=""
+              className="  h-[300px] block sm:hidden floating"
+            />
           </div>
         </div>
       </div>
-      <img src="images/coin-2.png" alt="" className=" absolute top-[20%] right-[-2%] floating" />
-
+      <img
+        src="images/coin-2.png"
+        alt=""
+        className=" absolute top-[20%] right-[-2%] floating"
+      />
     </section>
   );
 }
--- src/components/JoinNow.jsx
+++ src/components/JoinNow.jsx
@@ -7,7 +7,7 @@ function JoinNow() {
         <div className='joinus py-12 text-center my-5'>
             <h1 className=' text-3xl sm:text-6xl font-bitsu ' >Join The WHATBOT Community</h1>
 
-            <p className=' font-poppins pt-6 pb-10  text-[#8b8b8b]'>Meet the community of air drop farmers</p>
+          
             <a href="/" className="btn-main px-8 sm:px-16 py-4 mt-6 font-bitsu text-2xl sm:text-4xl">Join The Community</a>
 
         </div>

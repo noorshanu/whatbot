b0ed057a7cdca6c6835ce61f41a6e3d08e7f01f3
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-04 03:31:18 +0530

    new

--- src/components/Tokonomics.jsx
+++ src/components/Tokonomics.jsx
@@ -10,7 +10,7 @@ function Tokonomics() {
           </h1>
         </div>
 
-        <div className=" flex justify-center items-center gap-5">
+        <div className=" flex justify-center items-center gap-5 text-center">
           <div>
             <img
               src="images/token.png"
@@ -18,29 +18,34 @@ function Tokonomics() {
               className="h-auto sm:h-[500px]"
             />
 
-<div className="flex justify-center gap-7 items-center py-4 mt-8">
-             
+            <div className="flex justify-center gap-7 items-center pt-8 mt-8 font-bitsu text-base sm:text-3xl">
+              <h2> 50% Burn</h2>
 
-             
-                <h2> 50% Burn</h2>
+              <h2> 25% PinkSale</h2>
 
-                
-                <h2> 50% Burn</h2>
-                
-                <h2> 50% Burn</h2>
-                
-                <h2> 50% Burn</h2>
-                
-                <h2> 50% Burn</h2>
-                
-                <h2> 50% Burn</h2>
+              <h2> 12% Liquidty</h2>
+
+          
+
              
             </div>
-          </div>
 
-          <div>
+            <div className="flex justify-center gap-7 items-center py-1 mt-8 font-bitsu text-base sm:text-3xl">
+              <h2> 7% CEX</h2>
+
+              <h2> 6% Staking</h2>
+
+             
+
+          
+
+             
+            </div>
+
             
           </div>
+
+          <div></div>
         </div>
       </div>
     </section>

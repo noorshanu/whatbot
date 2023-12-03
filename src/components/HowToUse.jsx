108b0429372ba329f9006d9d7f3120238bfb26a6
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-04 01:29:33 +0530

    new

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -10,23 +10,53 @@ import SliderNavigationButton from "./SliderNavigationButton";
 const data = [
   {
     title: "setp 1",
-    subTitle: "Lorem, ipsum dolor sit amet consectetur aotam facilis autem sed  quae fugit sdfgdf gdfgsdfgdfshsfghh amet dolore neque esse. ",
+    subTitle: "Launch WhatBot: Begin your journey with WhatBot by accessing it on Telegram through the provided link. WhatBot is your all-in-one crypto companion designed to simplify the token creation and management process.  https://t.me/Launch_Whatbot ",
  
     
   },
   {
     title: "step 2",
-    subTitle: "Lorem, ipsum dolor sit amet consectetur aotam facilis autem sed  quae fugit sdfgdf gdfgsdfgdfshsfghh amet dolore neque esse.",
+    subTitle: "Choose Your Blockchain: When you enter WhatBot, you'll have the option to select your desired blockchain – whether it's Binance Smart Chain (BSC) or Ethereum (ETH). This choice gives you flexibility and access to different ecosystems.",
     
   },
   {
     title: "step 3",
-    subTitle: "Lorem, ipsum dolor sit amet consectetur aotam facilis autem sed  quae fugit sdfgdf gdfgsdfgdfshsfghh amet dolore neque esse.",
+    subTitle: "Token Creation: WhatBot makes token creation a breeze. It will prompt you with questions and guide you through the entire process. You'll provide essential information such as the token name, symbol, total supply, and other relevant details. WhatBot ensures that even if you're not a coding expert, you can bring your crypto vision to life.",
    
   },
   {
     title: "step 4",
-    subTitle: "Lorem, ipsum dolor sit amet consectetur aotam facilis autem sed  quae fugit sdfgdf gdfgsdfgdfshsfghh amet dolore neque esse.",
+    subTitle: "Wallet Generation or Integration: You have the option to generate a new wallet directly through WhatBot or integrate your existing wallet. This step is crucial for secure token deployment and management. ",
+
+    
+  },
+
+  {
+    title: "step 5",
+    subTitle: "Token Deployment: With your token details configured, WhatBot will assist you in deploying your token. The process is user-friendly, ensuring that your token creation journey is smooth and hassle-free. After deployment, it's essential to verify your token to ensure its authenticity",
+    
+    
+  },
+
+
+  {
+    title: "step 6",
+    subTitle: "Liquidity Setup: Setting up liquidity for your token on a decentralized exchange (DeX) can be a complex task, but not with WhatBot. It simplifies this process, allowing you to provide liquidity with just a few clicks. This step is vital for the liquidity and tradability of your token.",
+    
+    
+  },
+
+  {
+    title: "step 7",
+    subTitle: "Enable Trading: Once you've established liquidity, you can enable trading for your token. Now, your token is ready to be traded on the blockchain you've chosen. WhatBot ensures that your token is set up for success in the market.",
+    
+    
+  },
+
+  {
+    title: "step 8",
+    subTitle: "8. Further Customizations: Beyond these essential steps, WhatBot offers a /help menu for additional customizations and options. Whether you need to make adjustments, gather more information, or explore advanced features, WhatBot's /help menu is your go-to resource.",
+    
     
   },
 ];
--- src/components/JoinNow.jsx
+++ src/components/JoinNow.jsx
@@ -5,7 +5,7 @@ function JoinNow() {
    <section className=' relative z-50'>
     <div className='container-wrapper'>
         <div className='joinus py-12 text-center my-5'>
-            <h1 className=' text-3xl sm:text-6xl font-bitsu ' >Join The WHATBOT Community</h1>
+            <h1 className=' text-3xl sm:text-6xl font-bitsu  mb-8' >Join The WHATBOT Community</h1>
 
           
             <a href="/" className="btn-main px-8 sm:px-16 py-4 mt-6 font-bitsu text-2xl sm:text-4xl">Join The Community</a>
